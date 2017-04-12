27a1e0d36073e247ef9e1ac128d2121db7f8fda0
Alex Kocharin <alex@kocharin.ru>
2017-04-12 16:37:10 +0000

    Add nntp Article model

---
+++ models/nntp/Article.js
@@ -0,0 +1,40 @@
+// NNTP article
+//
+
+'use strict';
+
+
+const Mongoose = require('mongoose');
+const Schema   = Mongoose.Schema;
+
+
+module.exports = function (N, collectionName) {
+
+  let Article = new Schema({
+    source:  Schema.ObjectId,
+    group:   Schema.ObjectId,
+    index:   Number
+  }, {
+    versionKey: false
+  });
+
+
+  // Indexes
+  //////////////////////////////////////////////////////////////////////////////
+
+  // find an article by message_id (ARTICLE command)
+  Article.index({ source: 1 });
+
+  // get range of articles inside a group
+  Article.index({ group: 1, index: 1 });
+
+
+  N.wire.on('init:models', function emit_init_Article() {
+    return N.wire.emit('init:models.' + collectionName, Article);
+  });
+
+
+  N.wire.on('init:models.' + collectionName, function init_model_Article(schema) {
+    N.models[collectionName] = Mongoose.model(collectionName, schema);
+  });
+};
